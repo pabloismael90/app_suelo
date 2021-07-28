4a30ea515e7024f65302c00b9d73f97f928efedb
Pablo Garcia Murillo <pablo.ismael90@gmail.com>
2021-07-28 11:10:11 -0600

    finalizando cambios

--- lib/src/pages/PDFView.dart
+++ lib/src/pages/PDFView.dart
@@ -10,11 +10,9 @@ class PDFView extends StatelessWidget {
         return Scaffold(
             appBar: AppBar(),
             body: PDF.asset(
-            "assets/documentos/Instructivo suelo.pdf",
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            placeHolder: Image.asset("assets/icons/icon.png",
-                height: 200, width: 100),
+                "assets/documentos/Instructivo suelo.pdf",
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
             ),
         );
     }
